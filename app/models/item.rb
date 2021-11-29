30c21b73d29cb5bf159478f43b7fa583f39f1877
Louis-Lorgere <louis.lorgere@gmail.com>
2021-11-29 18:02:02 +0100

    the day before tomorrow

---
+++ app/models/item.rb
@@ -0,0 +1,11 @@
+class Item < ApplicationRecord
+  validates :title, :desciption, :price, :image_url, presence:true
+  validates :price, numericality: {greater_than: 0 }
+  
+  has_many :join_table_item_carts
+  has_many :carts, through: :join_table_item_carts
+  has_many :join_table_item_orders
+  has_many :orders, through: :join_table_item_orders
+  has_many :join_table_item_categories
+  has_many :categories, through: :join_table_item_categories
+end
