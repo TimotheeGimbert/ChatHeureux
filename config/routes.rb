7652a197c04ab3cf20905dac1adff0b947d5be7a
FabCom <fabcom@communs.fr>
2021-11-30 11:24:59 +0100

    Add profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'pages/contact'
   root 'pages#index'
   devise_for :users
+  resources :profiles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
