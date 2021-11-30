e203dff9a86199979da13906e17a5dfe8bf2e8d9
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-30 16:18:23 +0100

    new routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,12 @@ Rails.application.routes.draw do
   get 'pages/index'
   get 'pages/presentation'
   get 'pages/contact'
-  root 'pages#index'
+  
   resources :profiles
-  get 'orders/show'
-  get 'orders/new'
-  get 'orders/create'
-  get 'items/index'
-  get 'items/show'
-  get 'items/new'
-  get 'items/create'
-  get 'items/edit'
-  get 'items/update'
-  get 'items/delete'
+
+  root 'items#index'
+  resources :orders only: [:show, :new, :create]
+  resources :items only: [:index, :show, :new, :create, :edit, :update, :delete]
+
   devise_for :users
 end
