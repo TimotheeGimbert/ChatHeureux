017a621de485fd70f30a85ad166f7b3733b4b8c5
FabCom <fabcom@communs.fr>
2021-11-30 10:14:53 +0100

    Add _navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/index'
   get 'pages/presentation'
   get 'pages/contact'
+  root 'pages#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
