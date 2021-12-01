cae14e69bf1c21a3c157b4a903046111bf2abdac
TimotheeGimbert <timothee.gimbert@free.fr>
2021-12-01 16:07:50 +0100

    solving some issues

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,12 +2,20 @@ class CartsController < ApplicationController
 include CartsHelper
 before_action :is_sign?, :only_current_cart
 
-  def Show
+  def show
     @items_in_cart = JoinTableItemCart.where(cart_id:current_user.cart.id) #on va allé chercher dans la table de jointure les entrées ou le panier utilisé est le même que celui de la personne connecté, puis va enregistrer les entrées dans cette variable
   end
+
+  def create
+    
+  end
+
   def update
+
   end 
+
   def destroy
+
   end 
   
 end
