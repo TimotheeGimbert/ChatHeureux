e16a0951cd9f7ef525bff96c736e29d45a11aeb2
Paola <boxer.0006@live.fr>
2021-12-01 10:31:48 +0100

    bouton ajouter au panier + helper cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,7 @@
 class CartsController < ApplicationController
+include CartsHelper
+before_action :is_sign?, :only_current_cart
+
   def Show
     @items_in_cart = JoinTableItemCart.where(cart_id:current_user.cart.id) #on va allé chercher dans la table de jointure les entrées ou le panier utilisé est le même que celui de la personne connecté, puis va enregistrer les entrées dans cette variable
   end
