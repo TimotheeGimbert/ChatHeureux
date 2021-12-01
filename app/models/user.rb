ef073b9dc86c7fc021371be0f63ed8fe0b1a7bb7
Paola <boxer.0006@live.fr>
2021-12-01 11:34:45 +0100

    resolving conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
   
   has_many :orders
-  has_one :profile
+  has_one :profile, dependent: :destroy
   has_one :cart #user et cart on une relation N-1 
 end
