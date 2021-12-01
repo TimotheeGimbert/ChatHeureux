4d159cd2c8b489be0c9cacf6c9665aeffeeb6029
TimotheeGimbert <timothee.gimbert@free.fr>
2021-12-01 16:30:49 +0100

    creates method in JoinTable

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,12 +2,21 @@ class CartsController < ApplicationController
 include CartsHelper
 before_action :is_sign?, :only_current_cart
 
+  def new
+    @cart = Cart.new
+  end
+
   def show
-    @items_in_cart = JoinTableItemCart.where(cart_id:current_user.cart.id) #on va allé chercher dans la table de jointure les entrées ou le panier utilisé est le même que celui de la personne connecté, puis va enregistrer les entrées dans cette variable
+
   end
 
   def create
-    
+    @cart = Cart.new(user_id: params[:current_user_id])
+    if @cart.save 
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'Invalid cart parameters !'
+    end
   end
 
   def update
--- app/controllers/join_table_item_card_controller.rb
+++ app/controllers/join_table_item_card_controller.rb
@@ -1,4 +1,19 @@
 class JoinTableItemCardController < ApplicationController
+
+  def new
+    @cart = Cart.find_by(id: params[:cart_id])
+    @item_in_cart = JoinTableItemCart.new
+  end
+
+  def create
+    @item_in_cart = JoinTableItemCart.new(user_id: params[:current_user_id])
+    if @item_in_cart.save 
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'Invalid cart parameters !'
+    end
+  end
+
   private
   def parameters
     params.require(:join_table_item_carts).permit(:item_id, :card_id)
